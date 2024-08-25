33b490e9800d29ab4e0f55d32d0dec47c9359729
Ahmad Sandid <ahmad1702@gmail.com>
2024-08-25 16:53:49 -0500

    first commit

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,18 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {
+        keymaps = {
+          accept_suggestion = '<C-Enter>',
+          clear_suggestion = '<C-]>',
+          accept_word = '<C-j>',
+        },
+      }
+    end,
+  },
+}
